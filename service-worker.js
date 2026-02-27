49410ffca7f0439eae3560177ec9b92ebdcaeae5
lcmaurya <lal7590@gmail.com>
2026-02-27 12:42:35 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+// Service Worker generated via PWABuilder
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+const CACHE = 'pwabuilder-page';
+
+// Change this if you later add an offline.html page
+const offlineFallbackPage = 'index.html';
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('install', async (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.add(offlineFallbackPage))
+  );
+});
+
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      (async () => {
+        try {
+          const preloadResp = await event.preloadResponse;
+          if (preloadResp) {
+            return preloadResp;
+          }
+          return await fetch(event.request);
+        } catch (error) {
+          const cache = await caches.open(CACHE);
+          return await cache.match(offlineFallbackPage);
+        }
+      })()
+    );
+  }
+});
